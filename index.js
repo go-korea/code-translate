fc5d41bdf9c28c96ca3357bc1cb06e047d58f974
Winston Lee <winston@go-korea.com>
2024-04-26 17:34:20 +0900

    Working

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { Translate } = require('@google-cloud/translate').v2;
 // Your Google Cloud Platform project ID
 const projectId = 'hanquoc';
 const apiKey = process.env.API_KEY;
-const translate = new Translate({ projectId, key: apiKey });
+const googleTranslate = new Translate({ projectId, key: apiKey });
 
 const targetLanguage = "en";
 
@@ -28,17 +28,22 @@ async function iterate(node) {
     const text = node.rawText.trim();
     if (text) {
       // Translate the text to English
-      node.rawText = await translate.translate(text, {
+      let translateResult = await googleTranslate.translate(text, {
         to: targetLanguage
       });
+
+      // Replace the original text with the translated text
+      node.rawText = translateResult[0];
+
+      // Print the translated text to the console
       console.log(node.rawText);
     }
   } else {
-    node.childNodes.forEach(iterate);
+    await node.childNodes.forEach(iterate);
   }
 }
 
-iterate(root);
-
-// Write the translated HTML to a new file
-fs.writeFileSync('insurance-landing-' + targetLanguage + '.html', root.toString());
+iterate(root).then(() => {
+  // Write the translated HTML to a new file
+  fs.writeFileSync('insurance-landing-' + targetLanguage + '.html', root.toString());
+});
